24b1a832c864a5b8e269aac87b332fc092eb6caa
Oliver Kletzmayr <oliver@kletzmayr.com>
2018-04-12 03:58:40 +0200

    further logging tweaks, move scan function to utils

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import argparse
 import logging
 import sys
-from os import listdir, path
+from os import path
 
 import utils
 
-logFormatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+logFormatter = logging.Formatter('%(levelname)-5s - %(message)s')
 consoleHandler = logging.StreamHandler()
 consoleHandler.setFormatter(logFormatter)
 logger = logging.getLogger()
@@ -21,6 +21,7 @@ args = parser.parse_args()
 def main():
     if args.debug:
         fileHandler = logging.FileHandler('acsi-packager.log', mode='w')
+        fileHandler.setFormatter(logFormatter)
         logger.addHandler(fileHandler)
         logger.setLevel(logging.DEBUG)
 
@@ -36,53 +37,8 @@ def main():
         ac_install_dir = utils.get_ac_install_dir()
         logger.info('Detected installation: %s', ac_install_dir)
 
-    cars_dir = path.join(ac_install_dir, 'content', 'cars')
-    tracks_dir = path.join(ac_install_dir, 'content', 'tracks')
-    cars = 0
-    tracks = 0
+    utils.scan_ui_files(ac_install_dir)
 
-    logger.debug('Scanning for cars')
-    # scan every car in AC/content/cars
-    for car in listdir(cars_dir):
-        fp = path.join(path.join(cars_dir, car, 'ui', 'ui_car.json'))
-        info = utils.read_ui_file(fp)
-
-        logger.debug('car: "{}" ({})'.format(info['name'], car))
-        cars += 1
-
-    logger.debug('Scanning for tracks')
-    # scan every track in AC/content/tracks
-    for track in listdir(tracks_dir):
-        fp = path.join(path.join(tracks_dir, track, 'ui', 'ui_track.json'))
-
-        # this way we can avoid code redundancy
-        layouts = []
-
-        # if there's a toplevel ui_track.json, it's a single-layout track
-        if path.exists(fp):
-            info = utils.read_ui_file(fp)
-            tl = utils.fix_track_length(info['length'])
-            layouts.append({'name': info['name'],
-                            'path': track,
-                            'length': tl})
-
-        # if there are more, we have to scan each layout
-        else:
-            for track_config in listdir(path.join(tracks_dir, track, 'ui')):
-                if path.isdir(path.join(tracks_dir, track, 'ui', track_config)):
-                    fp = path.join(tracks_dir, track, 'ui', track_config, 'ui_track.json')
-                    info = utils.read_ui_file(fp)
-                    tp = track + '/' + track_config
-                    tl = utils.fix_track_length(info['length'])
-                    layouts.append({'name': info['name'],
-                                    'path': tp,
-                                    'length': tl})
-
-        for l in layouts:
-            logger.debug('track: "{}" ({}) {:.0f}m'.format(l['name'], l['path'], l['length']))
-        tracks += 1
-
-    logger.info('Found and parsed data of %d cars and %d tracks.', cars, tracks)
     print('We\'re done here. Press Enter or close this window.')
     input()
 
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import json
 import logging
 import winreg
-from os import path
+from os import listdir, path
 
 logger = logging.getLogger('main')
 
@@ -101,3 +101,53 @@ def read_ui_file(fp):
             info = json.load(f, strict=False)
 
     return info
+
+
+def scan_ui_files(ac_install_dir):
+    cars_dir = path.join(ac_install_dir, 'content', 'cars')
+    tracks_dir = path.join(ac_install_dir, 'content', 'tracks')
+    cars = 0
+    tracks = 0
+
+    logger.debug('Scanning for cars')
+    # scan every car in AC/content/cars
+    for car in listdir(cars_dir):
+        fp = path.join(path.join(cars_dir, car, 'ui', 'ui_car.json'))
+        info = read_ui_file(fp)
+
+        logger.debug('car: "{}" ({})'.format(info['name'], car))
+        cars += 1
+
+    logger.debug('Scanning for tracks')
+    # scan every track in AC/content/tracks
+    for track in listdir(tracks_dir):
+        fp = path.join(path.join(tracks_dir, track, 'ui', 'ui_track.json'))
+
+        # this way we can avoid code redundancy
+        layouts = []
+
+        # if there's a toplevel ui_track.json, it's a single-layout track
+        if path.exists(fp):
+            info = read_ui_file(fp)
+            tl = fix_track_length(info['length'])
+            layouts.append({'name': info['name'],
+                            'path': track,
+                            'length': tl})
+
+        # if there are more, we have to scan each layout
+        else:
+            for track_config in listdir(path.join(tracks_dir, track, 'ui')):
+                if path.isdir(path.join(tracks_dir, track, 'ui', track_config)):
+                    fp = path.join(tracks_dir, track, 'ui', track_config, 'ui_track.json')
+                    info = read_ui_file(fp)
+                    tp = track + '/' + track_config
+                    tl = fix_track_length(info['length'])
+                    layouts.append({'name': info['name'],
+                                    'path': tp,
+                                    'length': tl})
+
+        for l in layouts:
+            logger.debug('track: "{}" ({}) {:.0f}m'.format(l['name'], l['path'], l['length']))
+        tracks += 1
+
+    logger.info('Found and parsed data of %d cars and %d tracks.', cars, tracks)
